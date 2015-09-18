93a9e12f3aee30ce4a1755d40ee7b5ffdc70334c
Eugen Istoc <eugenistoc@gmail.com>
2015-09-18 01:29:13 -0400

    Add documentation

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "module.ParseBackend.ParseBackend",
+      "shortName": "module.ParseBackend.ParseBackend",
+      "type": "service",
+      "moduleName": "module.ParseBackend",
+      "shortDescription": "Provider for configuring the service",
+      "keywords": "angular api app app_id application application_id config configuring javascript javascript_key js_key key module parse parsebacked parsebackend parsebackendprovider provider service setapplicationid setjavascriptkey"
+    },
+    {
+      "section": "api",
+      "id": "Patcher",
+      "shortName": "Patcher",
+      "type": "service",
+      "moduleName": "module.ParseBackend",
+      "shortDescription": "ParseBackend provider",
+      "keywords": "api module parsebackend patcher provider service"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
